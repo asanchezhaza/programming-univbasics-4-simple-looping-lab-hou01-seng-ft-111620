c2adc5a0dee8d82ea1413d6aa7e0452acba8dcb8
Alan sanchez <asanchezhaza@gmail.com>
2020-10-14 08:00:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,12 @@
-# Write your methods here
+def loop_message_five_times(message)
+  counter <= 5
+  while loop_message_five_times[counter] do
+  puts loop_message_five_times[counter]
+  counter += 1
+end
+
+
+
+end 
+
+
