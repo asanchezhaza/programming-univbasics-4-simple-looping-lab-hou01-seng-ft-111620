3756d32508891cb294b5d4d8dd239456cf8d9c6b
Alan sanchez <asanchezhaza@gmail.com>
2020-10-14 08:22:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,3 +25,14 @@ def output_array(array)
 end
 
 
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
+  end
+  new_array
+end
+
+
